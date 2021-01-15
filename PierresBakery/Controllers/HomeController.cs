973fec626b1580a74a2c1e65cd27bea7ea140d43
Sarah Gilbert <sarahgilbert064@gmail.com>
2021-01-15 10:47:39 -0800

    add viewbags for both flavor and treat in home controller and home/index view

--- PierresBakery/Controllers/HomeController.cs
+++ PierresBakery/Controllers/HomeController.cs
@@ -1,13 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using PierresBakery.Models;
 
 namespace PierresBakery.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly PierresBakeryContext _db;
+
+    public HomeController(PierresBakeryContext db)
+    {
+      _db = db;
+    }
     
     [HttpGet("/")]
     public ActionResult Index()
     {
+      ViewBag.Flavors = _db.Flavors.ToList();
+      ViewBag.Treats = _db.Treats.ToList();
       return View();
     }
 
